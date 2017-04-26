efdc28cba53dcc460ce3b43fa1dd4e9d90a82aa0
rachelnicole <loveless@gmail.com>
2017-04-25 21:28:24 -0500

    removed excess code

--- sample.py
+++ sample.py
@@ -7,26 +7,10 @@ CF.Key.set(KEY)
 img_url = 'http://i.imgur.com/36Q3rNy.jpg'
 result = CF.face.detect(img_url)
 
-#this returns that the user group IS shown as my_anthem
-group = CF.person_group.get('my_anthem')
-
 faceNumber = result[0][u'faceId']
 
-faceList = [ faceNumber ]
-
-identifyPerson = CF.person.lists('my_anthem')
-
-
 # This compares our known faceId to our group
-whoIsShe = CF.face.identify(faceList, 'my_anthem', 1)
-
-print '-----------------'
-
-print faceNumber
-
-print '-----------------'
-
-print group
+whoIsShe = CF.face.identify([ faceNumber ], 'my_anthem', 1)
 
 print '-----------------'
 

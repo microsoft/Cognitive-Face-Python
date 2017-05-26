0526e7b8cb024dab70d65e02cc5a8ead1338dae8
huxuan <i@huxuan.org>
2017-05-26 14:46:21 +0800

    Add endpoint support.

--- sample/util.py
+++ sample/util.py
@@ -27,6 +27,7 @@ MAX_IMAGE_SIZE = 300
 MAX_THUMBNAIL_SIZE = 75
 STYLE = wx.SIMPLE_BORDER
 SUBSCRIPTION_KEY_FILENAME = 'Subscription.txt'
+ENDPOINT_FILENAME = 'Endpoint.txt'
 ORIENTATION_TAG = 274
 
 LOG_FACE_LIST_REQUEST = (
@@ -76,6 +77,40 @@ class SubscriptionKey(object):
         if os.path.isfile(SUBSCRIPTION_KEY_FILENAME):
             os.remove(SUBSCRIPTION_KEY_FILENAME)
         CF.Key.set(cls.key)
+
+
+class Endpoint(object):
+    """Endpoint."""
+
+    @classmethod
+    def get(cls):
+        """Get the endpoint."""
+        if not hasattr(cls, 'endpoint'):
+            cls.endpoint = ''
+        if not cls.endpoint:
+            if os.path.isfile(ENDPOINT_FILENAME):
+                with file(ENDPOINT_FILENAME) as fin:
+                    cls.endpoint = fin.read().strip()
+            else:
+                cls.endpoint = CF.BaseUrl.get()
+        CF.BaseUrl.set(cls.endpoint)
+        return cls.endpoint
+
+    @classmethod
+    def set(cls, endpoint):
+        """Set the endpoint."""
+        cls.endpoint = endpoint
+        with file(ENDPOINT_FILENAME, 'w') as fout:
+            print >>fout, endpoint
+        CF.BaseUrl.set(cls.endpoint)
+
+    @classmethod
+    def delete(cls):
+        """Delete the endpoint."""
+        cls.endpoint = ''
+        if os.path.isfile(ENDPOINT_FILENAME):
+            os.remove(ENDPOINT_FILENAME)
+        CF.BaseUrl.set(cls.endpoint)
 
 
 def scale_image(img, size=MAX_IMAGE_SIZE):

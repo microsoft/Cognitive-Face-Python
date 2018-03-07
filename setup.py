2068c1965ad26262921a64d921f91a205353dd1f
Sean Hu <Xuan.Hu@microsoft.com>
2018-03-07 07:47:30 +0000

    Merged PR 4361: Upgrade the version to 1.4.1.
    Upgrade the version to 1.4.1.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def readme():
 
 setup(
     name='cognitive_face',
-    version='1.4.0',
+    version='1.4.1',
     packages=find_packages(exclude=['tests']),
     install_requires=['requests'],
     author='Microsoft',
